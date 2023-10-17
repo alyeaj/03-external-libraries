1cf0d43c17b31a375aba012395f473a2a4155441
alyeaj <aj3744@nyu.edu>
2023-10-16 20:32:21 -0400

    destination to master
    took synth from html to sketch.js.
    changed toDestination() to toMaster().
    older version of tone.js

--- sketch.js
+++ sketch.js
@@ -8,3 +8,12 @@ function setup() {
 function draw() {
     background(109,80,90);
 }
+
+const synth = new Tone.Synth().toMaster();
+const playbutton = document.getElementById("play-button") 
+playbutton.addEventListener("click", play)
+
+function play(){
+    synth.triggerAttackRelease("C3", "2n", "F5", "2n") 
+    console.log(Tone.context.state)
+}
